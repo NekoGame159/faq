a6227a78dd3f837ab4f910f78edfb5ff010c5836
unknown <tima96504@gmail.com>
2021-12-25 11:00:47 +0500

    G

--- Practic/script.js
+++ Practic/script.js
@@ -2,7 +2,6 @@ function clickCube (){
 	var red = getRandomInt(255);
 	var green = getRandomInt(255);
 	var blue = getRandomInt(255);
-	var deg = getRandomInt(360);
 	var borderRadiusFirst =  getRandomInt(80);
 	var borderFirst = getRandomInt(9);
 
@@ -13,13 +12,11 @@ function clickCube (){
 	"0 0 200px rgba (" + red + ", " + green + ", " + blue +", 0.5 )," +
 	"0 0 300px rgba (" + red + ", " + green + ", " + blue +", 0.5 )";
 
-	document.getElementById ("pulse") .style.backgroundColor =" rgb (" + red + ", " + green + ", " + blue +")" ;
+	document.getElementById ("pulse") .style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue +")";
 	document.getElementById ("pulse") .style.boxShadow = stringBoxShadow;
-	document.getElementById ("container") . style.transform = "rotate (" + deg + " deg)";
 	document.getElementById ("pulse") .style.borderRadius = borderRadiusFirst + "px";
 	document.getElementById ("pulse") .style.border = borderFirst + "px solid";
 }
-
 function getRandomInt(max){
 	return Math.floor(Math.random() * max)
 }
